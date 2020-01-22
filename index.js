549656438e9762112727654a86923f196f93253c
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2020-01-22 12:28:51 -0600

    create a child window, an alert and open internet resources

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-const { app, shell, BrowserWindow } = require('electron')
+const { app, shell, BrowserWindow, ipcMain } = require('electron')
 
 let win
+let child
 
 function createWindow () {
   win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 850,
+    height: 650,
     webPreferences: {
       nodeIntegration: true
     },
     backgroundColor: '#A3BEC2'
   })
-
  
   win.loadFile('index.html')
-
   
-  win.webContents.on("new-window", function(event, url) {
+  win.webContents.on("new-window", function(event, url, frameName, disposition, options) {
+    
+    let urlRoute = url.split("/")
+    let resource = urlRoute[urlRoute.length-1]
     event.preventDefault();
-    shell.openExternal(url);
+    resource == "child.html" ? createChild() : shell.openExternal(url);
   });
 
+  win.webContents.on('did-finish-load', ()=>{
+    let code = `var alertButton = document.getElementById("alert-button");
+    alertButton.addEventListener("click",function(){alert("I'm learning Electron!!");});`;
+    win.webContents.executeJavaScript(code);
+  });
   
   win.on('closed', () => {
     
@@ -28,9 +35,28 @@ function createWindow () {
   })
 }
 
+function createChild () {
+
+  child = new BrowserWindow({
+    parent: win,
+    width: 600,
+    height: 400,
+    webPreferences: {
+      nodeIntegration: true
+    },
+    backgroundColor: '#763230'
+  })
+
+  child.loadFile('child.html')
+
+  child.on('closed', () => {
+    
+    child = null
+  })
+
+}
 
 app.on('ready', createWindow)
-
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
